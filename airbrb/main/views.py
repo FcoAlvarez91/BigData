d6dd77949adbbb0040eb4f1063c5e5ded7c46fab
ibparrague <ibparrague@miuandes.cl>
2021-06-07 18:58:04 -0400

    colours

--- airbrb/main/views.py
+++ airbrb/main/views.py
@@ -4,6 +4,7 @@ from .athena import pedirCosas
 
 from django.db.models import Sum
 from django.http import JsonResponse
+from random import randint
 # Create your views here.
 
 def home(response):
@@ -14,22 +15,31 @@ def home(response):
 def graph(response):
 	labels = []
 	data = []
+	rColours = []
 
 	for row in pedirCosas()[1:]:
 		labels.append(row[0])
 		data.append(row[1])
+		rColours.append([randint(0,255),randint(0,255),randint(0,255)])
 
 	print("graph 1")
 
-	return render(response, "main/graph.html", {"var1":123213,"labels":labels,"data":data})
+	return render(response, "main/graph.html", {"labels":labels,"data":data,"rColours":rColours})
 
 	
 def graph2(response):
-	labels = []
-	data = []
+	showData = []
 	
 	for row in pedirCosas()[1:]:
-		labels.append(row[0])
-		data.append(row[1])
+		showData.append([row[0],row[1], randint(0,255),randint(0,255),randint(0,255)])
+	print(showData)
 
-	return render(response, "main/graph2.html", {"var1":123213,"labels":labels,"data":data})
+
+	return render(response, "main/graph2.html", {"showData":showData})
+
+"""
+def random_int(a, b=None):
+	if b is None:
+		a, b = 0, a
+	return random.randint(a,b)
+"""
