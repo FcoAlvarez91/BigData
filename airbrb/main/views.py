14965301e3e6bec74977f5e087603b615f665fde
ibparrague <ibparrague@miuandes.cl>
2021-06-07 17:11:13 -0400

    Grafo 2 """"funcionando""""

--- airbrb/main/views.py
+++ airbrb/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .athena import pedirCosas
+
+from django.db.models import Sum
+from django.http import JsonResponse
 # Create your views here.
 
 def home(response):
@@ -16,4 +19,17 @@ def graph(response):
 		labels.append(row[0])
 		data.append(row[1])
 
+	print("graph 1")
+
 	return render(response, "main/graph.html", {"var1":123213,"labels":labels,"data":data})
+
+	
+def graph2(response):
+	labels = []
+	data = []
+	
+	for row in pedirCosas()[1:]:
+		labels.append(row[0])
+		data.append(row[1])
+
+	return render(response, "main/graph2.html", {"var1":123213,"labels":labels,"data":data})
