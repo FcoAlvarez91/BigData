93b5d0d1552b9f8cf97febd22cc30802834e3928
Francisco Alvarez <fcoalvarez91@gmail.com>
2021-06-08 20:43:26 -0400

    pull

--- airbrb/main/urls.py
+++ airbrb/main/urls.py
@@ -4,13 +4,10 @@ from . import views
 
 urlpatterns = [
 path("", views.home, name="home"),
-path("graph/", views.graph, name="graph"),
 path("index/", views.index, name="index"),
 path("city/", views.city, name="city"),
 path("city/options/", views.options, name="options"),
 path("city/options/something/", views.something, name="something"),
 path("AllCitiesGraphs/", views.AllCitiesGraphs, name="AllCitiesGraphs"),
-path("graph2/", views.graph2, name="graph2"),
-path("graph3/", views.graph4, name="graph3"),
 path("citydetail/", views.cityBudget, name="citydetail")
 ]
