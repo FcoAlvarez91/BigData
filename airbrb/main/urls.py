861c22afaf2d23c46def43fb8fdfeb099c8bcf38
Francisco Alvarez <fcoalvarez91@gmail.com>
2021-06-08 23:34:05 -0400

    Final look

--- airbrb/main/urls.py
+++ airbrb/main/urls.py
@@ -3,11 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("", views.home, name="home"),
-path("index/", views.index, name="index"),
+path("", views.index, name="index"),
 path("city/", views.city, name="city"),
 path("city/options/", views.options, name="options"),
-path("city/options/something/", views.something, name="something"),
 path("AllCitiesGraphs/", views.AllCitiesGraphs, name="AllCitiesGraphs"),
 path("citydetail/", views.cityBudget, name="citydetail")
 ]
